c5ff4d1921c6a53fb95dfedd616b2a2d6f074ea7
parkshs <tgrat920515@gmail.com>
2023-10-07 14:11:38 +0900

    #empty miniproject 실습 - 할일관리 앱 만들기

---
+++ miniproject/src/component/TodoItem.js
@@ -0,0 +1,28 @@
+import "./TodoItem.css";
+
+const TodoItem = ({ id, content, isDone, createdDate, onUpdate, onDelete }) => {
+  const onChangeCheckbox = () => {
+    onUpdate(id);
+  };
+
+  const onDeleteItem = () => {
+    onDelete(id);
+  };
+
+  return (
+    <div className="TodoItem">
+      <div className="checkbox_col">
+        <input type="checkbox" checked={isDone} onChange={onChangeCheckbox} />
+      </div>
+      <div className="title_col">{content}</div>
+      <div className="date_col">
+        {new Date(createdDate).toLocaleDateString()}
+      </div>
+      <div className="btn_col">
+        <button onClick={onDeleteItem}>삭제</button>
+      </div>
+    </div>
+  );
+};
+
+export default TodoItem;
---
+++ miniproject/src/component/TodoList.js
@@ -0,0 +1,43 @@
+import "./TodoList.css";
+import TodoItem from "./TodoItem";
+import { useState } from "react";
+
+const TodoList = ({ todo, onUpdate, onDelete }) => {
+  const [search, setSearch] = useState("");
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getSearchResult = () => {
+    console.log(todo);
+    return search === ""
+      ? todo
+      : todo.filter((it) =>
+          it.content.toLowerCase().includes(search.toLowerCase())
+        );
+  };
+
+  return (
+    <div className="TodoList">
+      <h4>Todo List✨</h4>
+      <input
+        value={search}
+        onChange={onChangeSearch}
+        className="searchbar"
+        placeholder="검색어를 입력하세요"
+      />
+      <div className="list_wrapper">
+        {getSearchResult().map((it) => (
+          <TodoItem
+            key={it.id}
+            {...it}
+            onUpdate={onUpdate}
+            onDelete={onDelete}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TodoList;
